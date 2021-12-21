33e4fa0266f9b15a398dae7b147d93c6dbe040d6
calebbartley <calebbartley18@gmail.com>
2021-12-20 19:39:05 -0500

    Components added + Nav to give test look and feel of site. dont think i will be using the nav elements moving forward. Too restrictive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+    <Router>
+    <div >
+      <div className="--" >
+      </div>
+      <Navbar/>
+      <Home/>
+      <Routes className="--">
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Portfolio" component={Portfolio} />
+      </Routes>
+      
+    </div>
+  </Router>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height:'100vh',
+
+        }}>
+            <h1 className='Text'>
+                <h3>
+                Spoken word artist , Talor’d Tales, was born in Saint Louis , Missouri in 1994. She developed a passion for creative writing at an early age ; she loved the freedom of constructing a concept that expressed her imagination. Through her educational career, she excelled in English, winning writing and speech competitions, while also writing and editing papers for classmates. Though she excelled in school,  she experienced various avenues of mental abuse through boys bullying and parental figures,  leading her to attempt suicide in 2015. In this dark era, poetry found her.  A notebook became her therapist while a pen became her voice allowing her to vocalize her depression. She later channeled her trauma from being body shamed to embracing her body and sexuality. An alchemist, she transmuted those painful encounters into her weapons of empowerment by writing and performing erotic poetry. After a failed relationship, she found herself lacking pleasure from her partner leading her to live through others’ sexual desires, birthing Talor’d Tales, LLC. Both avenues became the foundation of shifting the patriarchy paradigm. Since a child, she has always challenged the traditional norm of conduct  and conformity to gender roles.
+                Talor’d  gives a voice to those who are liberated and sexually expressive, slowly but surely erasing the stigma of sex as taboo.
+                </h3>
+            </h1>
+        </div>
+    )
+}
+
+export default Home
