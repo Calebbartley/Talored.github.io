51b4896ce261eae1f73260cff57d21f26b424d68
calebbartley <calebbartley18@gmail.com>
2021-12-29 22:51:45 -0500

    working navigation and pages set up. Whats left is styling and adding features.'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
 import Home from './Components/homePage/Home';
 import About from './Components/aboutMe/About';
 import Portfolio from './Components/portfolio/Portfolio';
-import Navmenu from './Components/navigation/Navbar';
+import NavBar from './Components/navigation/Navbar';
 import Contact from './Components/contact/Contact';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 
 function App() {
   return (
-    <div className="body">
-      <Navmenu />
-      <div>
-        <div className="pages"></div>
-        <Portfolio className="ribs"/>
-        <About className="ribs"/>
-        <Contact className="ribs"/>
-        <Home className="ribs"/>
-      </div>
+    <div className="app">
+    <h1>Talored Tales</h1>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/About" element={<About/>} />
+        <Route exact path="Portfolio/" element={<Portfolio/>} />
+        <Route exact path="/Contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+
     </div>
   );
 }
